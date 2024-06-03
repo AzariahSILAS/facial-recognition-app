ea466ce2ad73917314b35d393e3bc93790c21c95
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2024-06-02 20:22:45 -0700

    im just getting ready to get into the hard part so im making sure to make a save before i potentially mess it all up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-
+import React, { Component } from 'react'
 import './App.css';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-function App() {
-  return (
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onButtonSubmite = () => {
+    console.log('click')
+  }
+  render() {
+    return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+       onInputChange={this.onInputChange} 
+       onButtonSubmite={this.onButtonSubmite} 
+       />
       {/* 
       <Faceerecognition /> */}
     </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
