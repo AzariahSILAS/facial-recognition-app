36b0db5d84714aa60104443a6c435bc419f670fc
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2024-06-03 17:16:29 -0700

    still brockan but i want to save it before i start deleting to much stuff

--- src/App.js
+++ src/App.js
@@ -11,6 +11,42 @@ const app = new Clarifai.App({
   apiKey: 'API Key'
 })
 
+const returnClearifiaiJSONRequestOptions = (imageUrl) => {
+  const PAT = '7734c39d98144ad9a42d07993498c3e1';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'azariahsilas';       
+const APP_ID = 'facial-recognition';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';   
+const IMAGE_URL = imageUrl;
+
+const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+});
+requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+return requestOptions
+
+
 
 class App extends Component {
   constructor() {
@@ -24,11 +60,15 @@ class App extends Component {
     this.setState({input: event.target.value});
   }
   onButtonSubmite = () => {
+
+
     this.setState({imageUrl: this.state.input})
     console.log('click')
-    App.models.predict(
-       Clarifai.FACE_DETECT_MODEL,
-       this.state.input)
+    // App.models.predict(
+    //    Clarifai.FACE_DETECT_MODEL,
+    //    this.state.input)
+     fetch("https://api.clarifai.com/v2/models/" + FACE_DETECT_MODEL + "/outputs", returnClearifiaiJSONRequestOptions(this.state.input))
+    .then(response => response.json())
       .then(
       function(response) {
 
